14658de78c59e1334898315ed08a8cca48e552f5
sumanmanna134 <manna.suman134@gmail.com>
2023-07-21 23:43:26 +0530

    docker-compose setup, mongodb-primary

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -3,6 +3,6 @@ import { OrdersModule } from './orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
-  await app.listen(3000);
+  await app.listen(9900);
 }
 bootstrap();
